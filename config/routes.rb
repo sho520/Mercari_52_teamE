7abce37023ccc861c04b183ef3c1671811628e78
Kazuaki-Tsuruta <turutakazuaki@icloud.com>
2019-06-09 13:43:34 +0900

    add_route&views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :amas
+  resources :turus
+  resources :tech_camp_mentor_abes
+
 end
