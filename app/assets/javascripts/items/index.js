1f8d837cfa909f845cf67f320dff8a65985f9bde
S <42575166+shinpei555@users.noreply.github.com>
2019-06-11 16:44:47 +0900

    bxslider jsファイル追加

---
+++ app/assets/javascripts/items/index.js
@@ -0,0 +1,16 @@
+<script>
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+       auto: true,
+       slideWidth: 760,
+       pause:	3000,
+       speed: 300,
+       mode: 'fade',
+       pager:true,
+       controls:true,
+       minSlides: 1,
+       maxSlides: 1,
+       touchEnabled: true,
+     });
+});
+</script>
