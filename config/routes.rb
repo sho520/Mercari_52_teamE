63f95eb4cb2a935feafb3423a8fcbf8321c68e61
S <42575166+shinpei555@users.noreply.github.com>
2019-06-11 15:28:56 +0900

    gem 'bxslider-rails'追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :amas
   resources :turus
-  resources :tech_camp_mentor_abes
   resources :items
   resources :users
   get "items/confirm" => "items#confirm"  #idが入る必要あり
