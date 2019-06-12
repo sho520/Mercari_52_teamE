743c3df64829d2d7f828b660df23b3ece998e5a1
Dice0907 <dais.f07@gmail.com>
2019-06-12 19:14:09 +0900

    Resistration_form_pre_fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,18 +13,23 @@ class UsersController < ApplicationController
   end
 
   def index
+    render :layout => 'third_application'
   end
 
   def phone
+    render :layout => 'third_application'
   end
   
   def address
+    render :layout => 'third_application'
   end
     
   def purchase
+    render :layout => 'third_application'
   end
     
   def complete
+    render :layout => 'third_application'
   end
 
 end
