59fc23fc8057f07f83a82c953e32f8a64c066953
S <42575166+shinpei555@users.noreply.github.com>
2019-06-14 02:52:02 +0900

    商品購入ページ実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "items/confirm" => "items#confirm"  #idが入る必要あり
+  get "items/confirm/done" => "items#done"  #idが入る必要あり
   get "items/brand" => "items#brand"
   get "items/category" => "items#category"
   get "users/phone" => "users#phone"
