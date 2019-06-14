5e96d3b6acf8046552fd043e400dabf6f2d55e03
Dice0907 <dais.f07@gmail.com>
2019-06-14 15:46:30 +0900

    all

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,27 +1,27 @@
 class ItemsController < ApplicationController
 
   def index
-    render :layout => 'top'
+    render layout: 'top'
   end
 
   def show
-    render :layout => 'application'
+    render layout: 'application'
   end
 
   def new
-    render :layout => 'second_application'
+    render layout: 'second_application'
   end
 
   def confirm
-    render :layout => 'second_application'
+    render layout: 'second_application'
   end
 
   def brand
-    render :layout => 'application'
+    render layout: 'application'
   end
 
   def category
-    render :layout => 'application'
+    render layout: 'application'
   end
 
 end
