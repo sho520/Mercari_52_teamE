1ec6288073ce5351731593920b9c185fee64ce5a
tanakanat <tr14919120@gmail.com>
2019-06-17 15:25:42 +0900

    検索結果のアイテムに詳細ページのlinkを付与

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,13 @@ class SearchController < ApplicationController
   
   def search
     @keyword = params[:keyword]
-    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%")
+    if @keyword == ""
+      @items = Item.limit(16).order('id DESC')
+    else
+      @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%")
+    end
+    # binding pry
+    
     render layout: 'application'
   end
 
