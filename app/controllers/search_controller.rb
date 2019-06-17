a8ca79ab1ed6f3c8a68a92f97ba15c323ddf8d2b
tanakanat <tr14919120@gmail.com>
2019-06-17 16:39:29 +0900

    検索ワードに対しての切り分け

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,14 +2,18 @@ class SearchController < ApplicationController
   
   def search
     @keyword = params[:keyword]
-    if @keyword == ""
-      @items = Item.limit(16).order('id DESC')
-    else
-      @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%")
+
+    if @keyword.empty?
+      @items = Item.order('id DESC') and return
+    end
+
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order('id DESC')
+
+    if @items.empty?
+      @items = Item.order('id DESC')
+      @not_find = "該当する商品が見つかりません。検索条件を変えて、再度お試しください。" and return
     end
     # binding pry
-    
-    render layout: 'application'
   end
 
 end
