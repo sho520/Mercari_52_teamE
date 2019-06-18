0a91b41a4cbc0bc4e6cbf761a1c5f1ee3ed2aefa
Dice0907 <dais.f07@gmail.com>
2019-06-18 16:44:26 +0900

    devise_user_setup

--- db/migrate/20190618055155_delete.rb
+++
@@ -1,5 +0,0 @@
-class Delete < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_060352) do
+ActiveRecord::Schema.define(version: 2019_06_18_064647) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -112,12 +112,13 @@ ActiveRecord::Schema.define(version: 2019_06_18_060352) do
     t.string "email", default: ""
     t.string "encrypted_password", default: ""
     t.string "phone"
-    t.integer "prefecture_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile"
+    t.text "credit"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
