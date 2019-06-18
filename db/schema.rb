bf386ae88247d9baa24cc3ec0f0215ebccaa178e
Dice0907 <dais.f07@gmail.com>
2019-06-18 17:05:24 +0900

    gem uninstall-simple_form

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_06_18_064647) do
 
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -27,7 +26,6 @@ ActiveRecord::Schema.define(version: 2019_06_18_064647) do
   end
 
   create_table "delivery_ways", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "delivery_way", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
