d4c540b0ea733ed01b2b03165015abb521f50ecd
tanakanat <tr14919120@gmail.com>
2019-06-18 19:40:06 +0900

    コントローラー修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,18 +4,20 @@ class SearchController < ApplicationController
     @keyword = params[:keyword]
 
     if @keyword.empty?
-      @items = Item.order('id DESC').includes(:images) 
+      @items = Item.all.order('id DESC')
+
       render layout: 'common' and return
     end
 
     @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order('id DESC')
 
     if @items.empty?
-      @items = Item.order('id DESC')
+      @items = Item.all.order('id DESC')
       @not_find = "該当する商品が見つかりません。検索条件を変えて、再度お試しください。"
       render layout: 'common' and return
     end
-    render layout: 'common' and return
+
+    render layout: 'common'
   end
 
 end
