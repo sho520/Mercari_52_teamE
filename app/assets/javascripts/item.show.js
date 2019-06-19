52ef20d419e6165f38144e97a44aa25be7047f3e
Kazuaki-Tsuruta <turutakazuaki@icloud.com>
2019-06-19 14:38:46 +0900

    remove_unnecessary_log

--- app/assets/javascripts/item.show.js
+++ app/assets/javascripts/item.show.js
@@ -1,8 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $(".product__item-box__main__left__below__pics").on("mouseover", function(){
-    console.log("a")
     var src = $(this).children('img').attr('src');
-    console.log(src)
            $("#bigimage").attr("src",src);
     return false;
   });
