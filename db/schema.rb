6b04187f88b8326829de07474802754cd006c728
tanakanat <tr14919120@gmail.com>
2019-06-19 14:57:23 +0900

    検索バーにclassを追加

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_06_18_064647) do
   end
 
   create_table "delivery_ways", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "delivery_way", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
