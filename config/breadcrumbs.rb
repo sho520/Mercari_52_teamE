af7538cbb6efeaea1822bb5ee48ac91998769756
tanakanat <tr14919120@gmail.com>
2019-06-20 14:57:00 +0900

    users周りのパンくずを設定

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -9,6 +9,26 @@ end
 
 # users関連
 crumb :mypage do
-  link "マイページ", user_path
+  link "マイページ", "/users/1"
+end
+
+crumb :profile do
+  link "プロフィール", edit_user_path
+  parent :mypage
+end
+
+crumb :identification do
+  link "本人情報の登録", users_identification_path
+  parent :mypage
+end
+
+crumb :card do
+  link "支払い情報", users_card_path
+  parent :mypage
+end
+
+crumb :logout do
+  link "ログアウト", users_logout_path
+  parent :mypage
 end
 
