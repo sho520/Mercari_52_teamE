380f99fd4c5b4e82b576ee1a4b2d62f5ab4f4d0d
Dice0907 <dais.f07@gmail.com>
2019-06-24 14:28:28 +0900

    SNS Log_in: Controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,24 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
-  # callback for facebook
   def facebook
-    callback_for(:facebook)
+    callback_from :facebook
   end
 
-  # callback for google
   def google_oauth2
-    callback_for(:google)
+    callback_from :google
   end
 
-  # common callback method
-  def callback_for(provider)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+  private
+  def callback_from(provider)
+    provider = provider.to_s
+    omi = request.env['omniauth.auth']
+    @user = User.find_for_oauth(omi)
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
-  end
-
-  def failure
-    redirect_to root_path
   end
 
 end
