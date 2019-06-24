29fff9279970ba3cce1ee3024b84963e9c1f3411
S <42575166+shinpei555@users.noreply.github.com>
2019-06-24 15:24:55 +0900

    ヘッダー修正完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_075418) do
+ActiveRecord::Schema.define(version: 2019_06_24_030422) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2019_06_21_075418) do
   end
 
   create_table "delivery_ways", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "delivery_way", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -127,6 +126,7 @@ ActiveRecord::Schema.define(version: 2019_06_21_075418) do
     t.datetime "updated_at", null: false
     t.text "profile"
     t.text "credit"
+    t.string "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
