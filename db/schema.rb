f85d35bb9e3d90e9a7f85037ba2efc35b14cc1b0
S <42575166+shinpei555@users.noreply.github.com>
2019-06-24 15:51:28 +0900

    userModel修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_030422) do
+ActiveRecord::Schema.define(version: 2019_06_22_132640) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
