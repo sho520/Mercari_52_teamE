5e7332af460a64538f3b4f623d473bfe816eb2f5
Dice0907 <dais.f07@gmail.com>
2019-06-26 16:04:56 +0900

    sns_log_in_fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,34 @@ class User < ApplicationRecord
 
   mount_uploader :image_url, ImageUploader
 
-  def self.find_for_oauth(auth, signed_in_resource=nil)
-    sns = SnsCredential.where(uid: auth.uid, provider: auth.provider).first
-    unless sns
-      @user = User.create(
-      nickname:    auth.info.name,
-      email:    auth.info.email,
-      password: Devise.friendly_token[0,20]
-      )
-      sns = SnsCredential.create(
-      user_id: @user.id,
-      uid: auth.uid,
-      provider: auth.provider
-      )
-   end
-    sns
-    @user
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    if snscredential.present?
+      user = User.where(id: snscredential.user_id).first
+    else
+      user = User.where(email: auth.info.email).first
+      if user.present?
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      else
+        user = User.create(
+          nickname: auth.info.name,
+          email:    auth.info.email,
+          password: Devise.friendly_token[0, 20],
+          telephone: "08000000000"
+          )
+        SnsCredential.create(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+          )
+      end
+    end
+    return user
   end
 end
