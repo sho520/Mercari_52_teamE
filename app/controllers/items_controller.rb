ebf3914e5da742317d1ad7c20c0e62fb36cf0b4e
sho520 <shoa.xyz@gmail.com>
2019-06-27 19:27:18 +0900

    size 出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -122,7 +122,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :category_id,:prefecture_id, :description, :condition_id, :shipping_fee_payer_id, :shipping_day_id, :price, images_attributes:[:id,:image_url]).merge(owner_id: current_user.id)
+    params.require(:item).permit(:name, :size_id, :category_id,:prefecture_id, :description, :condition_id, :shipping_fee_payer_id, :shipping_day_id, :price, images_attributes:[:id,:image_url]).merge(owner_id: current_user.id)
 
   end
 
