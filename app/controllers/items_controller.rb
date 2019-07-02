dc1ce8f338a2a637fc114598e6a4d4b0f72a0a55
sho520 <shoa.xyz@gmail.com>
2019-07-02 20:06:24 +0900

    出品　view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :confirm, :pay, :done, :buy]
-  before_action :authenticate_user!, only: [:new]
+  # before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.page(params[:page]).per(16).includes(:images)
@@ -139,7 +139,8 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :state_id, :size_id,:large_class_id,:middle_class_id, :category_id,:prefecture_id, :description, :condition_id, :shipping_fee_payer_id, :shipping_day_id, :price, images_attributes:[:id,:image_url]).merge(owner_id: current_user.id)
+    params.require(:item).permit(:name, :state_id, :size_id,:large_class_id,:middle_class_id, :category_id,:prefecture_id, :description, :condition_id, :shipping_fee_payer_id, :shipping_day_id, :price, images_attributes:[:id,:image_url])
+    # .merge(owner_id: current_user.id)
   end
 
   def image_params
